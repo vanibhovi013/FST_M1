32257558f6a3e9cdc490b5a6052c18dba8643655
vanibhovi013 <90038754+vanibhovi013@users.noreply.github.com>
2022-05-06 19:36:20 +0530

    Add files via upload

---
+++ JUnit/Activities/Plane.java
@@ -0,0 +1,42 @@
+package activities;
+
+import examples.Array;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Plane {
+
+    private List<String> passengers;
+    private int maxPassengers;
+    private Date lastTimeTookOf;
+    private Date lastTimeLanded;
+
+    public Plane(int maxPassengers){
+        this.maxPassengers = maxPassengers;
+        this.passengers = new ArrayList<>();
+    }
+
+    public void onBoard(String passenger){
+        this.passengers.add(passenger);
+    }
+
+    public Date takeOff(){
+        this.lastTimeTookOf = new Date();
+        return lastTimeTookOf;
+    }
+
+    public void land(){
+        this.lastTimeLanded = new Date();
+        this.passengers.clear();
+    }
+
+    public Date getLastTimeLanded(){
+        return lastTimeLanded;
+    }
+
+    public List<String> getPassengers(){
+        return passengers;
+    }
+}
